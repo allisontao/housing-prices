2e9a64cf8e0a2c7352bffd9c5fc13e00fa57a049
cccc237 <71087772+cccc237@users.noreply.github.com>
2023-07-07 12:14:22 -0400

    edited UI

--- app.py
+++ app.py
@@ -3,30 +3,36 @@ import pandas as pd
 import numpy as np
 import pickle
 
+st.title('House Price Prediction')
+st.sidebar.header('House Data')
+
+
 def main():
   with st.form(key='form'):
-    LotArea = st.text_input("LotArea")
-    BedroomAbvGr = st.text_input("BedroomAbvGr")
-    HalfBath = st.text_input("HalfBath")
-    GrLivArea = st.text_input("GrLivArea")
-    OverallQual = st.text_input("OverallQual")
-    KitchenAbvGr = st.text_input("KitchenAbvGr")
-    TotRmsAbvGrd = st.text_input("TotRmsAbvGrd")
-    BsmtHalfBath = st.text_input("BsmtHalfBathr")
-    FullBath = st.text_input("FullBath")
-    BsmtFullBath = st.text_input("BsmtFullBath")
-    MoSold = st.text_input("MoSold")
-    OverallCond = st.text_input("OverallCond")
-    YearBuilt = st.text_input("YearBuilt")
-    ThreeSsnPorch = st.text_input("3SsnPorch")
-    ScreenPorch = st.text_input("ScreenPorch")
-    LowQualFinSF = st.text_input("LowQualFinSF")
-    YearRemodAdd = st.text_input("YearRemodAdd")
-    GarageArea = st.text_input("GarageArea")
-    EnclosedPorch = st.text_input("EnclosedPorch")
-    FirstFlrSF = st.text_input("1stFlrSF")
-    SecondFlrSF = st.text_input("2ndFlrSF")
-    GarageCars = st.text_input("GarageCars")
+    OverallQual = st.sidebar.selectbox('Select rating for overall quality:',(1,2,3,4,5,6,7,8,9,10))
+    MoSold = st.sidebar.selectbox('Select Month Sold:',(1,2,3,4,5,6,7,8,9,10,11,12)) 
+    OverallCond = st.sidebar.selectbox('Select Overall Condition Rating:',(1,2,3,4,5,6,7,8,9,10)) 
+    BsmtHalfBath = st.sidebar.selectbox('Select Basement half bathrooms:',(0,1,2)) 
+
+    LotArea = st.sidebar.number_input('LotArea', step=1) 
+    BedroomAbvGr = st.sidebar.number_input('BedroomAbvGr', step=1) 
+    HalfBath = st.sidebar.number_input('HalfBath', step=1) 
+    GrLivArea = st.sidebar.number_input('GrLivArea', step=1)  
+    KitchenAbvGr = st.sidebar.number_input('KitchenAbvGr', step=1)  
+    TotRmsAbvGrd = st.sidebar.number_input('TotRmsAbvGrd', step=1) 
+    FullBath = st.number_input('FullBath', step=1) 
+    BsmtFullBath = st.sidebar.number_input('BsmtFullBath', step=1)  
+    YearBuilt = st.sidebar.number_input('YearBuilt', min_value= 1600, max_value=2023, value= 2000, step=1)
+    ThreeSsnPorch = st.sidebar.number_input('3SsnPorch', step=1)
+    ScreenPorch = st.sidebar.number_input('ScreenPorch', step=1)
+    LowQualFinSF = st.sidebar.number_input('LowQualFinSF', step=1)
+    YearRemodAdd = st.sidebar.number_input('YearRemodAdd', step=1)
+    GarageArea = st.sidebar.number_input('GarageArea', step=1)
+    EnclosedPorch = st.sidebar.number_input('EnclosedPorch', step=1)
+    FirstFlrSF = st.sidebar.number_input('1stFlrSF', step=1)
+    SecondFlrSF = st.sidebar.number_input('2ndFlrSF', step=1)
+    GarageCars = st.sidebar.number_input('GarageCars', step=1)
+    
     submit_button = st.form_submit_button(label='Enter')
 
     if submit_button:
@@ -56,8 +62,13 @@ def main():
       }
 
       data = pd.DataFrame(user_data, index=[0])
-      model = pickle.load(open('model.sav', 'rb'))
-      price = model.predict(data)
-      st.write('Predicted house price is: $'+str(price[0]))
+      return data
+    
+  
+house_data = main()
+st.write(house_data)
 
-main()
+model = pickle.load(open('model.sav', 'rb'))
+price = model.predict(house_data)
+st.subheader('Predicted house price:')
+st.subheader('$'+str(np.round(price[0],2)))
